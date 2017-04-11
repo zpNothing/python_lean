df6a556fb166213a6a73ec181a49f8d3efffa806
zpNothing <173343021@qq.com>
2017-04-11 21:24:05 +0800

    init

---
+++ nester/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nester',
+    version='1.0.0',
+    py_modules=['nester'],
+    author='zppython',
+    author_email='173343021@qq.com',
+    url='https://github.com/zpNothing/python_lean',
+    description='A simple printer of nested lists',
+)
